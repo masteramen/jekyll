416d38d6864d79a38ac3b2520a4279e308868732
alexwang <2919798881@qq.com>
2018-10-14 20:29:29 +0800

    update source

--- build.sh
+++
@@ -1,1 +0,0 @@
-JEKYLL_ENV=production jekyll build --config site/_config.yml -s site  -d blog  -w
--- sh/build.sh
+++ sh/build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 export JEKYLL_ENV=production 
-jekyll build --config /site/_config.yml -s /site  -d /blog 
-cd /blog && git commit -am 'auto commit $(date) && git push
+bundle exec jekyll build --config /site/_config.yml -s /site  -d /blog 
+cd /blog && git add . &&  git commit -am 'auto commit $(date)' && git push
