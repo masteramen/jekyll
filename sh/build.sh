5e79be1fe74b8ce4afc3705b12a65decdcd6723e
Tester <2917798881@qq.com>
2018-10-17 22:36:37 +0800

    fixed date var

--- sh/build.sh
+++ sh/build.sh
@@ -3,4 +3,4 @@ export JEKYLL_ENV=production
 cd /blog && git pull
 bundle exec jekyll build --config /site/_config.yml -s /site  -d /blog --inc 
 cd /blog && git add . 
-cd /blog &&  git commit -am 'auto commit $(date)' && git push
+cd /blog &&  git commit -am "auto commit $(date)" && git push
