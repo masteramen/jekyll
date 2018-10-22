f582849badfe2083345fc20d237b26be06e93e20
alexwang <2919798881@qq.com>
2018-10-22 20:52:21 +0800

    auto commit Mon Oct 22 2018 20:52:21 GMT+0800 (CST)

--- sh/build_local.sh
+++ sh/build_local.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 export JEKYLL_ENV=production 
-cd ~/git/blog && git pull
+cd ~/git/site && git pull
 cd ~/git/jekyll
 bundle exec jekyll build --config ~/git/jekyll/_config.yml -s ~/git/jekyll  -d ~/git/site --inc 
 cd ~/git/site && git add . 
